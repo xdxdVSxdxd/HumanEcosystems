b8cb522c2c3f9729a60663204f8540aeff8bb69c
Salvatore <Iaconesi@Salvatores-MacBook-Pro.local>
2016-06-30 18:22:38 +0200

    added index files to root directory

---
+++ HE_v2/index.php
@@ -0,0 +1,25 @@
+<html>
+<head>
+<title>Human Ecosystems</title>
+<link rel="stylesheet" type="text/css" href="style.css">
+</head>
+<body>
+	<div class="page">
+		<div class="header">
+			<h1>Human Ecosystems</h1>
+		</div>
+		<div class="row">
+			<div class="rowcontent">
+				<ul>
+					<li>
+						<a href="installation/index.php" title="Main Installation/Configuration page">Main Installation/Configuration page</a>
+					</li>
+					<li>
+						<a href="visualizations" title="visualizations">Visualizations</a>
+					</li>
+				</ul>
+			</div>
+		</div>
+	</div>
+</body>
+</html>
